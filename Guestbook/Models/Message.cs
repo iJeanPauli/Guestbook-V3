41f773ccaf4bce87fd6e56c8928ae65f13dc50b2
Jean-Paul Boersma <1630091boersma@zuyd.nl>
2018-05-14 09:25:04 +0200

    First upload
    First upload of the whole repo

---
+++ Guestbook/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Guestbook.Models
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string TheMessage { get; set; }
+    }
+
+    public class MessageDBContext : DbContext
+        { 
+            public DbSet<Message> Messages { get; set; }
+        }
+}
